a9dcffcbaaffb6a79b486950eea5daa24cd412b5
Bill Niakas <billniakas@gmail.com>
2017-07-28 21:54:11 +0300

    fixed folder issue

--- file_count.py
+++ file_count.py
@@ -3,26 +3,28 @@ import os.path
 
 print(os.getcwd())
 
-
+folder=input("Give a path : ")
 def file_count(folder):
     while True:
-        dir = "./"+folder
+        dir = folder
+        
         if dir == '': break
         if os.path.isdir(dir):
             count_files = 0
             for r,d,f in os.walk(dir):
                 level = r.replace(dir, '').count(os.sep)
-                print(level*'\t',r)
+                
+                print(level*'\t',r,"\r")
                 for fi in f:
                     if fi[0] not in '.~':
-                        print((level+1)*'\t',fi)
+                        print((level+1)*'\t',fi,"\r")
                         count_files += 1
         print('{} files found'.format(count_files))
         break
 
 
 
-# folder="1920x1080"
+
 
 file_count(folder)
     
