c4696511da30356805ebaa9ba4a281968d74437e
Bill Niakas <billniakas@gmail.com>
2017-07-29 08:15:16 +0300

    added gpu support for Ubuntu 14.04

--- osprober.py
+++ osprober.py
@@ -100,6 +100,10 @@ else:
     computer=user+"@"+hostname
     underline=str(len(computer)*"-")
     gpu = subprocess.getoutput("glxinfo -B | grep -i '''opengl renderer string:'''")
+    if len(gpu) == 0:
+        gpu = subprocess.getoutput("lspci -k | grep -iEA3 '3d|vga compatible controller' | head -n1")
+        gpu = re.findall(r'\[(.*?)\]',gpu) 
+        gpu = " ".join(gpu)
     
 ##    linuxtype=re.findall('\w{4,}',platform[2], re.I)
 ##    if len(linuxtype)==0 or linuxtype[0]=="generic":
@@ -113,11 +117,12 @@ else:
     memfree = float(subprocess.getoutput("grep MemFree /proc/meminfo |grep -oE '''[0-9]*'''"))/pow(10,6)
     memactive = (memtotal-memfree)
     uptime = subprocess.getoutput("uptime -p")
-    cpu = subprocess.getoutput("cat /proc/cpuinfo | grep '''model name''' | head -n1").replace("          "," ").replace("  @ ","@")
+    cpu = subprocess.getoutput("cat /proc/cpuinfo | grep '''model name''' | head -n1").replace("  @ ","@")
+    cpu = " ".join(re.findall(r': (.*)',cpu)).replace("    "," ")
     
     #print(" ".join(ostype))
     os.system("clear")		
-    print("\n",os_dict[linuxtype[5:].lower()][1].format(computer,underline,resolution,os_dict[linuxtype[5:].lower()][0],platform[2],desktop,memtotal,memfree,memactive,uptime[3:],cpu[13:],packagelist[0],gpu[24:]))
+    print("\n",os_dict[linuxtype[5:].lower()][1].format(computer,underline,resolution,os_dict[linuxtype[5:].lower()][0],platform[2],desktop,memtotal,memfree,memactive,uptime[3:],cpu,packagelist[0],gpu))
     #print("\n"*2)
     
 
