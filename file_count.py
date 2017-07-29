6925ec98de36fb8a9d9a2d89dc5804286bee87b0
billniakas <billniakas@gmail.com>
2017-07-29 08:28:17 +0300

    file_count fix

--- file_count.py
+++ file_count.py
@@ -5,20 +5,29 @@ print(os.getcwd())
 
 folder=input("Give a path : ")
 def file_count(folder):
+    homefolder="~/"
+    if folder == "~/" and len(folder)==2:
+            folder = "/home/"
+    if homefolder in folder:
+            folder="/home/"+folder.replace("~/","")
+    
+    
     while True:
         dir = folder
-        
+               
         if dir == '': break
-        if os.path.isdir(dir):
-            count_files = 0
-            for r,d,f in os.walk(dir):
-                level = r.replace(dir, '').count(os.sep)
-                
-                print(level*'\t',r,"\r")
-                for fi in f:
-                    if fi[0] not in '.~':
-                        print((level+1)*'\t',fi,"\r")
-                        count_files += 1
+        
+        else:
+            if os.path.isdir(dir):
+                count_files = 0
+                for r,d,f in os.walk(dir):
+                    level = r.replace(dir, '').count(os.sep)
+                    
+                    print(level*'\t',r)
+                    for fi in f:
+                        if fi[0] not in '.~':
+                            print((level+1)*'\t',fi,"\r")
+                            count_files += 1
         print('{} files found'.format(count_files))
         break
 
